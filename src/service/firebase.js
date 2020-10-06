b978320836bf219259746ad17cab960f8546ee53
dev-woong <occhoi90@gmail.com>
2020-10-07 06:12:32 +0900

    project set up & add firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
